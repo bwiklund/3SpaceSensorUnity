5056895c8d3b0a0b2906962c228286c12d9098e5
Ben Wiklund <ben@daisyowl.com>
2015-07-02 20:10:31 -0700

    unity usage

---
+++ Assets/ImuBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImuBehaviour : MonoBehaviour {
+  IImu imu;
+
+	void Start () {
+    imu = new ThreeSpaceImu();
+    imu.Start();
+	}
+
+  void OnApplicationQuit() {
+    imu.Stop();
+  }
+
+	void Update () {
+    transform.rotation = imu.LatestRotation;
+	}
+}
