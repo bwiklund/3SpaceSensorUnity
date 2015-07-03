e37a37ffbb30c541f3acc3c38effa5dd69d7fa0c
Ben Wiklund <ben@daisyowl.com>
2015-07-02 20:12:44 -0700

    formatting

--- Assets/ImuBehaviour.cs
+++ Assets/ImuBehaviour.cs
@@ -4,16 +4,16 @@ using System.Collections;
 public class ImuBehaviour : MonoBehaviour {
   IImu imu;
 
-	void Start () {
+  void Start() {
     imu = new ThreeSpaceImu();
     imu.Start();
-	}
+  }
 
   void OnApplicationQuit() {
     imu.Stop();
   }
 
-	void Update () {
+  void Update() {
     transform.rotation = imu.LatestRotation;
-	}
+  }
 }
